97466679dc22c1de5a7f4744e59fbb2fcf52cc28
Zane Swafford <zane@otters.io>
2016-06-20 16:17:31 -0500

    Added the remaining base styles and scripts

--- lib/whiskers/generator.rb
+++ lib/whiskers/generator.rb
@@ -39,24 +39,19 @@ module Whiskers
     end
     
     desc 'watch', 'Watch a Whiskers directory and live compile coffeescript and SASS files'
-    method_options :path => :string
     def watch
-      puts "Watching for coffeescript and SASS changes #{options[:path]}"
-      puts 'Press enter to stop.'
+      puts "Watching for coffeescript and SASS changes. Press enter to stop.\n\n"
       
-      coffee_thread = Thread.new do
-        system("cd #{options[:path]} && " +
-               "coffee --watch --output scripts/lib --compile scripts/src")
-      end
+      coffee_pid = spawn("coffee --watch --output scripts/lib --compile scripts/src")
+      sass_pid = spawn("sass --watch stylesheets/app.sass:stylesheets/app.css")
       
-      sass_thread = Thread.new do
-        system("cd #{options[:path]} && " + 
-               "sass --watch stylesheets/app.sass:stylesheets/app.css")
-      end
+      enter_key = $stdin.gets.chomp
 
-      gets
-      coffee_thread.kill
-      sass_thread.kill
+      Process.kill("KILL", coffee_pid)      
+      Process.kill("KILL", sass_pid)
+      Process.wait
+      
+      puts "\nCompilation Stopped."
     end
 
     private
@@ -70,6 +65,7 @@ module Whiskers
       
       install_stylesheets_for_template_in_directory(template, directory)
       install_scripts_for_template_in_directory(template, directory)
+      install_markup_for_template_in_directory(template, directory)
       
       install_bourbon_in_directory directory
       install_neat_in_directory directory
@@ -84,22 +80,31 @@ module Whiskers
     end
     
     def install_stylesheets_for_template_in_directory(template, directory)
-      FileUtils.cp_r(all_stylesheets_for_template(template), directory)
+      FileUtils.cp_r(stylesheets_directory_for_template(template), directory)
     end
 
     def install_scripts_for_template_in_directory(template, directory)
-       install_path = File.join(directory, 'scripts')
-       FileUtils.cp_r(all_scripts_for_template(template), directory)
+      FileUtils.cp_r(scripts_directory_for_template(template), directory)
+    end
+    
+    def install_markup_for_template_in_directory(template, directory)
+      FileUtils.cp_r(markup_files_for_template(template), directory)
     end
 
     def install_bourbon_in_directory directory
       install_path = File.join(directory, 'stylesheets', 'plugins')
+      unless File.directory?(install_path)
+        FileUtils.mkdir_p(install_path)
+      end
       g = Bourbon::Generator.new([],{path: install_path})
       g.install
     end
         
     def install_neat_in_directory directory
       install_path = File.join(directory, 'stylesheets', 'plugins')
+      unless File.directory?(install_path)
+        FileUtils.mkdir_p(install_path)
+      end
       ## Neat's install method doesn't allow you to set the path :(  
       #g = Neat::Generator.new([],{path: install_path})
       #g.install
@@ -107,18 +112,30 @@ module Whiskers
     end
     
     def install_bitters_in_directory directory
+      ## Bitters doesn't name it's install directory bitters. It names it base by defualt.
+      ## So we rename it to 'bitters' here.
       install_path = File.join(directory, 'stylesheets', 'plugins')
+      unless File.directory?(install_path)
+        FileUtils.mkdir_p(install_path)
+      end
       g = Bitters::Generator.new([],{path: install_path})
       g.install
+      FileUtils.mv(File.join(install_path, 'base'), File.join(install_path, 'bitters'))
     end
     
     def install_normalize_in_directory directory
       install_path = File.join(directory,
                                'stylesheets', 
-                               'plugins', 
-                               'normalize.scss')
-      normalize_url = 'https://github.com/necolas/normalize.css/blob/master/normalize.css'
-      File.open(install_path, 'wb') do |f|
+                               'plugins')
+      
+      unless File.directory?(install_path)
+        FileUtils.mkdir_p(install_path)
+      end
+      
+      file_path = File.join(install_path, 'normalize.scss')
+      
+      normalize_url = 'https://raw.githubusercontent.com/necolas/normalize.css/master/normalize.css'
+      File.open(file_path, 'wb') do |f|
         f.write open(normalize_url).read 
       end
     end
@@ -127,28 +144,48 @@ module Whiskers
       install_path = File.join(directory, 
                                'scripts', 
                                'lib', 
-                               'plugins', 
-                               'jquery.min.js')
+                               'plugins')
+      
+      unless File.directory?(install_path)
+        FileUtils.mkdir_p(install_path)
+      end
+      
+      file_path = File.join(install_path, 'jquery.min.js')
+      
       jquery_url = 'https://code.jquery.com/jquery-3.0.0.min.js'
-      File.open(install_path, 'wb') do |f|
+      File.open(file_path, 'wb') do |f|
         f.write open(jquery_url).read 
       end
     end
-
-    def all_stylesheets_for_template template
-      Dir["#{stylesheets_directory_for_template template}/*"]
+    
+    def install_require_in_directory directory
+      install_path = File.join(directory, 
+                               'scripts', 
+                               'lib',
+                               'plugins')
+      
+      unless File.directory?(install_path)
+        FileUtils.mkdir_p(install_path)
+      end
+      
+      file_path = File.join(install_path, 'require.min.js')
+      
+      require_url = 'http://requirejs.org/docs/release/2.2.0/minified/require.js'
+      File.open(file_path, 'wb') do |f|
+        f.write open(require_url).read 
+      end
     end
 
     def stylesheets_directory_for_template template
       File.join(top_level_directory, 'core', 'whiskers', template, 'stylesheets')
     end
 
-    def all_scripts_for_template template
-      Dir["#{scripts_directory_for_template template}/*"]
-    end
-
     def scripts_directory_for_template template
       File.join(top_level_directory, 'core', 'whiskers', template, 'scripts')
+    end
+    
+    def markup_files_for_template template
+      Dir["#{File.join(top_level_directory, 'core', 'whiskers', template)}/*.html"]
     end
 
     def top_level_directory
